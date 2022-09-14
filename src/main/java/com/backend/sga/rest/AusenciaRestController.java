d06763d547b9f6d878c4168ca9a52d74db8fcef0
GabrielMarques011 <gmgabriellima@gmail.com>
2022-09-14 15:57:37 -0300

    Criamos os arquivos RestControllers (mais por enquanto sem metodos)

---
+++ src/main/java/com/backend/sga/rest/AusenciaRestController.java
@@ -0,0 +1,13 @@
+package com.backend.sga.rest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//CrossOrigin serve para que o projeto receba JSON
+@CrossOrigin
+@RestController
+@RequestMapping("/api/ausencia")
+public class AusenciaRestController {
+
+}
