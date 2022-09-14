3ba53d12ebc9f8b37e39fa9c5bf93e3cb3f9413e
GabrielMarques011 <gmgabriellima@gmail.com>
2022-09-13 22:20:02 -0300

    Iniciando projeto back-end, juntamente com: Paulo e Runbens

---
+++ src/main/java/com/backend/sga/SgaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.sga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaApplication.class, args);
+	}
+
+}
