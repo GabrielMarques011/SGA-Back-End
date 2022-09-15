730772096115984a721e78252a5a044b2715dc36
GabrielMarques011 <gmgabriellima@gmail.com>
2022-09-15 18:57:56 -0300

    Mexendo no RestAmbiente e no Repository

--- src/main/java/com/backend/sga/repository/AmbienteRepository.java
+++ src/main/java/com/backend/sga/repository/AmbienteRepository.java
@@ -1,5 +1,8 @@
 package com.backend.sga.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,7 @@ import com.backend.sga.model.Ambiente;
 @Repository
 public interface AmbienteRepository extends PagingAndSortingRepository<Ambiente, Long>{
 
+	//select * from sga.ambiente;
+	//public List<Ambiente> findAllAmbientes();
+	
 }
--- src/main/java/com/backend/sga/rest/AmbienteRestController.java
+++ src/main/java/com/backend/sga/rest/AmbienteRestController.java
@@ -1,11 +1,22 @@
-package com.backend.sga.rest;
+package com.backend.sga.rest;""
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
+import com.backend.sga.model.Ambiente;
 import com.backend.sga.repository.AmbienteRepository;
+
+import br.com.senaisp.sistemaauditorio.model.Agendamento;
 
 //CrossOrigin serve para que o projeto receba JSON
 @CrossOrigin
@@ -15,5 +26,22 @@ public class AmbienteRestController {
 
 	@Autowired
 	private AmbienteRepository ambienteRepository;
+	
+	@RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> criarAmbiente (@RequestBody Ambiente ambiente, HttpServletRequest request){
+			ambienteRepository.save(ambiente);
+	}
+	
+	//metodo para excluir por 'ID'
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Object> excluirAmbiente (@PathVariable("id") Long id, Ambiente ambiente,HttpServletRequest request){// Verificando se o id do 'ambiente' é igual ao do passado
+		ambienteRepository.deleteById(id);
+	}
+	
+	//Buscando todos os dados no banco
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public Iterable<Ambiente> listaAmbiente (Ambiente ambiente){
+		ambienteRepository.findAll();
+	}
 	
 }
