3d30ea7e5cf7ec9f1a1ef6b8acd9ba21490fafcb
GabrielMarques011 <gmgabriellima@gmail.com>
2022-09-20 20:41:19 -0300

    aplicando a criptografia na senha do Usuario

--- src/main/java/com/backend/sga/SgaApplication.java
+++ src/main/java/com/backend/sga/SgaApplication.java
@@ -2,12 +2,17 @@ package com.backend.sga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//estamos desabilitando os endpoints para que cada alteração não tenha que ficar fazendo login
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SgaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgaApplication.class, args);
 	}
+	
+	
+	//o annotation Bean serve para que consiga carregar possiveis alterações dentro do metodo
 
 }
--- src/main/java/com/backend/sga/rest/UsuarioRestController.java
+++ src/main/java/com/backend/sga/rest/UsuarioRestController.java
@@ -1,11 +1,16 @@
 package com.backend.sga.rest;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,10 +32,20 @@ public class UsuarioRestController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	//passando o parametro do BCrypto dentro encoder para que salva a criptografia
+	private PasswordEncoder encoder = new BCryptPasswordEncoder();
+	
 	//metodo para criar o professor
 	@RequestMapping(value = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> criarUser (@RequestBody Usuario user, HttpServletRequest request){
 		if(user != null) {
+			
+			//pegando a senha e transformando em criptografia
+			String crip = this.encoder.encode(user.getSenha());
+			
+			//mandando criptografada
+			user.setSenha(crip);
+			
 			usuarioRepository.save(user);
 			Sucesso sucesso = new Sucesso(HttpStatus.OK, "Sucesso");
 			return new ResponseEntity<Object>(sucesso, HttpStatus.OK);
@@ -66,10 +81,42 @@ public class UsuarioRestController {
 			Erro erro = new Erro(HttpStatus.INTERNAL_SERVER_ERROR, "NIF inválido", null);
 			return new ResponseEntity<Object>(erro, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-			//usuarioRepository.findById(nif);
+		
+			//pegando a senha e transformando em criptografia
+			String crip = this.encoder.encode(user.getSenha());
+			
+			//mandando criptografada
+			user.setSenha(crip);
+		
 			usuarioRepository.save(user);
 			Sucesso sucesso = new Sucesso(HttpStatus.OK, "Sucesso");
 			return new ResponseEntity<Object>(sucesso, HttpStatus.OK);
+	}
+	
+	
+	//metodo feito para validar quando criamos o login
+	//metodo feito para comparar se a senha bate com a do banco de dados
+	public Boolean validarSenha (Usuario user){
+		//pegando a senha do banco de dados
+		String senha = usuarioRepository.findById(user.getNif()).get().getSenha();
+		//pegando a senha do banco e comparando com a atual
+		Boolean valid = encoder.matches(user.getSenha(), senha);
+		return valid;
+	}
+	
+	//metodo para verificar se o login do Usuario esta correto
+	@RequestMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> login (@RequestBody Usuario user, HttpServletRequest request){
+		//trazendo valores do valid do 'validarSenha'
+		Boolean valid = validarSenha(user);
+		//verificando se a senha foi digitada corretamente
+		if (!valid) {
+			Erro erro = new Erro(HttpStatus.INTERNAL_SERVER_ERROR, "Login ou Senha incorreta", null);
+			return new ResponseEntity<Object>(erro, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		//mensagem de OK, caso esteja tudo certo
+		Sucesso sucesso = new Sucesso(HttpStatus.OK, "Sucesso ao Logar");
+		return new ResponseEntity<Object>(sucesso, HttpStatus.OK);
 	}
 	
 }
