268ad860d2bbdb441daa561ed5ede3edd4c59489
GabrielMarques011 <gmgabriellima@gmail.com>
2022-10-21 11:03:24 -0300

    aplicando um RecebeBuscaAmbiente

---
+++ src/main/java/com/backend/sga/model/RecebeBuscaAmbiente.java
@@ -0,0 +1,20 @@
+package com.backend.sga.model;
+
+import java.util.Calendar;
+
+import lombok.Data;
+
+@Data
+public class RecebeBuscaAmbiente {
+
+	private Ambiente ambiente;
+
+	private Periodo periodo;
+
+	private Calendar dataInicio;
+
+	private Calendar dataFinal;
+
+	private boolean diasSemana[];
+	
+}
--- src/main/java/com/backend/sga/repository/AmbienteRepository.java
+++ src/main/java/com/backend/sga/repository/AmbienteRepository.java
@@ -1,6 +1,8 @@
 package com.backend.sga.repository;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -12,28 +14,33 @@ import com.backend.sga.model.Periodo;
 import com.backend.sga.model.TipoAmbiente;
 
 @Repository
-public interface AmbienteRepository extends PagingAndSortingRepository<Ambiente, Long>{
+public interface AmbienteRepository extends PagingAndSortingRepository<Ambiente, Long> {
 
-	//select * from sga.ambiente;
-	//public List<Ambiente> findAllAmbientes();
-	
-	//SELECT * FROM sga.ambiente WHERE sga.ambiente.nome LIKE '%15%'
+	// select * from sga.ambiente;
+	// public List<Ambiente> findAllAmbientes();
+
+	// SELECT * FROM sga.ambiente WHERE sga.ambiente.nome LIKE '%15%'
 	@Query("SELECT a FROM Ambiente a WHERE a.nome LIKE %:nome%")
 	public List<Ambiente> palavraChave(@Param("nome") String nome);
-	
-	//SELECT * FROM sga.ambiente WHERE sga.ambiente.tipo_ambiente = 1
+
+	// SELECT * FROM sga.ambiente WHERE sga.ambiente.tipo_ambiente = 1
 	@Query("SELECT a FROM Ambiente a WHERE a.tipoAmbiente = :tipo_ambiente")
-	public Iterable<Ambiente> buscaAmbiente (@Param("tipo_ambiente") TipoAmbiente tipoAmbiente);
-	
-	//SELECT * FROM sga.ambiente WHERE sga.ambiente.capacidade between 15 AND 30;
+	public Iterable<Ambiente> buscaAmbiente(@Param("tipo_ambiente") TipoAmbiente tipoAmbiente);
+
+	// SELECT * FROM sga.ambiente WHERE sga.ambiente.capacidade between 15 AND 30;
 	@Query("SELECT a FROM Ambiente a WHERE a.capacidade BETWEEN :capacidadeMin AND :capacidadeMax")
-	public Iterable<Ambiente> retornaCapacidade (@Param("capacidadeMin") int capacidadeMin, @Param("capacidadeMax") int capacidadeMax);
-	
-	//SELECT * FROM sga.ambiente WHERE sga.ambiente.tipo_ambiente = 2 AND sga.ambiente.capacidade BETWEEN 20 AND 30
+	public Iterable<Ambiente> retornaCapacidade(@Param("capacidadeMin") int capacidadeMin,@Param("capacidadeMax") int capacidadeMax);
+
+	// SELECT * FROM sga.ambiente WHERE sga.ambiente.tipo_ambiente = 2 AND sga.ambiente.capacidade BETWEEN 20 AND 30
 	@Query("SELECT a FROM Ambiente a WHERE a.tipoAmbiente = :tipo_ambiente AND a.capacidade BETWEEN :capacidadeMin AND :capacidadeMax")
-	public Iterable<Ambiente> retornaTipoCapacidade (@Param("tipo_ambiente") TipoAmbiente tipoAmbiente, @Param("capacidadeMin") int capacidadeMin, @Param("capacidadeMax") int capacidadeMax);
-	
-	
-	
-	
+	public Iterable<Ambiente> retornaTipoCapacidade(@Param("tipo_ambiente") TipoAmbiente tipoAmbiente,@Param("capacidadeMin") int capacidadeMin, @Param("capacidadeMax") int capacidadeMax);
+
+	// SELECT a.* FROM sga.ambiente as a inner join sga.aula as au on a.id = au.ambiente_id where au.data >= "2022-10-24" AND au.data <= "2022-11-08" and au.periodo = 0 group by ID ;
+
+	@Query("SELECT a FROM Ambiente a INNER JOIN Aula au ON a.id = au.ambiente.id WHERE au.data >= :datainicio AND au.data <= :dataFinal AND au.periodo = :periodo")
+	public List<Ambiente> retornaOcupados(@Param("datainicio") Calendar dataInicio, @Param("dataFinal") Calendar dataFinal, @Param("periodo") Periodo periodo);
+
+	@Query("SELECT a FROM Ambiente a INNER JOIN Aula au ON a.id = au.ambiente.id WHERE au.data = :datainicio AND au.periodo = :periodo")
+	public Optional<Ambiente> retornaOcupadosDia(@Param("datainicio") Calendar dataInicio, @Param("periodo") Periodo periodo);
+
 }
