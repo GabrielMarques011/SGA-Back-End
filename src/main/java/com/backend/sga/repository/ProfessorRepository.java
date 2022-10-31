5179333eeb7d0af13741661dda8e1441ae9a5f04
GabrielMarques011 <gmgabriellima@gmail.com>
2022-10-31 19:32:28 -0300

    Feito algumas das Querys pedidas

--- src/main/java/com/backend/sga/repository/ProfessorRepository.java
+++ src/main/java/com/backend/sga/repository/ProfessorRepository.java
@@ -1,17 +1,13 @@
 package com.backend.sga.repository;
 
-import java.util.Calendar;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.backend.sga.model.Periodo;
 import com.backend.sga.model.Professor;
-import com.backend.sga.model.UnidadeCurricular;
 
 @Repository
 public interface ProfessorRepository extends PagingAndSortingRepository<Professor, Long>{
@@ -22,5 +18,10 @@ public interface ProfessorRepository extends PagingAndSortingRepository<Professo
 	public List<Professor> palavraChave(@Param("nome") String nome);
 	
 	// SELECT p.* FROM sga.professor as p inner join sga.competencia as c on p.id = c.professor_id inner join sga.aula as a on a.professor_id = p.id where c.unidade_curricular_id = 1 and a.periodo = 1 and a.data = "2022-10-05"
+	
+	//SELECT * FROM sga.professor order by sga.professor.nome asc;
+	@Query("SELECT p FROM Professor p ORDER BY p.nome ASC")
+	public List<Professor> orderProf();
+	
 	
 }
