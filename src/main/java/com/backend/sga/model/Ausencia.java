af8b8e016a7ba2bae8e4b880c0ad3446b50a4020
Rebeca Almeida <almeidarebeca944@gmail.com>
2022-11-09 10:32:31 -0300

    Metodo de disponibilidade Ambiente atualizado (Mobile)

--- src/main/java/com/backend/sga/model/Ausencia.java
+++ src/main/java/com/backend/sga/model/Ausencia.java
@@ -7,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
--- src/main/java/com/backend/sga/model/Professor.java
+++ src/main/java/com/backend/sga/model/Professor.java
@@ -2,21 +2,14 @@ package com.backend.sga.model;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import lombok.Data;
 
